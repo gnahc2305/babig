58d960cec5be490d960b0f964016e35de77669b8
gnahc2305 <achangmurillo@gmail.com>
2022-11-14 16:36:52 -0500

    create wave and button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="container">
+        <h1>Feliz Cumpleaños!!!!</h1>
+        <button type="button">Clickeame</button>
+
+        <div class="custom-shape-divider-top-1668461604">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+              class="shape-fill"
+            ></path>
+          </svg>
+        </div>
+
+        
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
