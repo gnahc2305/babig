7f0ffa4077279fb11d10ec0145a11f8fa7f4a363
gnahc2305 <achangmurillo@gmail.com>
2022-11-14 16:53:35 -0500

    add timer section

---
+++ src/components/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Timer() {
+  return (
+    <div className='timer'>
+        <h2>Nos volvemos a ver en: </h2>
+        <p>10 min</p>
+    </div>
+  )
+}
+
+export default Timer
