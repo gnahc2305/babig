1bbd08fe1261b1ae571f34e5b669537aafbd3970
gnahc2305 <achangmurillo@gmail.com>
2022-11-15 19:47:41 -0500

    added images

--- src/components/Fotos.js
+++ src/components/Fotos.js
@@ -1,27 +1,72 @@
-import React from 'react'
+import React from "react";
+import p1 from '../assets/Pictures/p1.jpeg'
+import p2 from '../assets/Pictures/p2.jpeg'
+import p3 from '../assets/Pictures/p3.jpeg'
+import p4 from '../assets/Pictures/p4.jpeg'
+import p5_s from '../assets/Pictures/p5.jpeg'
+import p6 from '../assets/Pictures/p6.jpeg'
+import p7 from '../assets/Pictures/p7.jpeg'
+import p8 from '../assets/Pictures/p8.jpeg'
+import p9 from '../assets/Pictures/p9.jpeg'
+import p10 from '../assets/Pictures/p10.jpeg'
+import p11_s from '../assets/Pictures/p11.jpeg'
+import p12 from '../assets/Pictures/p12.jpeg'
+import p13 from '../assets/Pictures/p13.jpeg'
+import p14 from '../assets/Pictures/p14.jpeg'
+import p15_s from '../assets/Pictures/p15.jpeg'
 
 function Fotos() {
   return (
     <>
-      <div className="fotos-container">
-        <div className="picture">1</div>
-        <div className="picture">2</div>
-        <div className="picture">3</div>
-        <div className="picture">4</div>
-        <div className="picture">5</div>
-        <div className="picture">6</div>
-        <div className="picture">7</div>
-        <div className="picture">8</div>
-        <div className="picture">9</div>
-        <div className="picture">10</div>
-        <div className="picture">11</div>
-        <div className="picture">12</div>
-        <div className="picture">13</div>
-        <div className="picture">14</div>
-        <div className="picture">15</div>
+      <div className="gallery">
+        <div className="picture">
+          <img src={p5_s} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p11_s} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p15_s} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p1} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p2} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p3} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p4} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p6} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p7} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p8} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p9} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p10} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p12} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p13} alt="" />
+        </div>
+        <div className="picture">
+          <img src={p14} alt="" />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Fotos
+export default Fotos;
